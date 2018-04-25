51a60853c89c06ff71acfd9b22027ec091082a78
0ddlyoko <0ddlyokoOfficial@gmail.com>
2018-04-25 21:00:37 +0200

    Default project

---
+++ web/README.md
@@ -0,0 +1,4 @@
+NitroCMS
+========
+
+A Symfony project created on April 25, 2018, 6:35 pm.
---
+++ web/launchServer.bat
@@ -0,0 +1,3 @@
+@echo off
+echo Lancement du serveur ... Vous pouvez le rejoindre sur http://localhost:8000
+php bin/console server:run
